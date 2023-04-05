92e8ef1b6e9a46f56f190c1a26eede52fde27df1
Nathan <nluiten1@ewu.edu>
2023-04-05 15:23:07 -0700

    hashCode, equals, and compareTo

--- src/cscd212classes/Television.java
+++ src/cscd212classes/Television.java
@@ -1,6 +1,8 @@
 package cscd212classes;
 
-public class Television {
+import java.util.Objects;
+
+public class Television implements Comparable<Television>{
     private final boolean fourK;
     private final String make;
     private final String model;
@@ -22,12 +24,15 @@ public class Television {
         if(this.resolution == 2160) {
             this.fourK = true;
         }
+        else {
+            this.fourK = false;
+        }
     }
 
-    public Television(final String model, final boolean smart, final int screenSize, final int resolution, final String make, boolean fourK) {
+    /*public Television(final String model, final boolean smart, final int screenSize, final int resolution, final String make, boolean fourK) {
 
         new Television(make, model, smart, screenSize, resolution);
-    }
+    }*/
 
     public String getMake() {
         return make;
@@ -55,4 +60,34 @@ public class Television {
         }
         return getMake() + "-" + getModel() + ", " + getScreenSize() + "inch " + smart + "tv with " + resolution + " resolution";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Television that = (Television) o;
+        return fourK == that.fourK && getResolution() == that.getResolution() && getScreenSize() == that.getScreenSize() && smart == that.smart && Objects.equals(getMake(), that.getMake()) && Objects.equals(getModel(), that.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fourK, getMake(), getModel(), getResolution(), getScreenSize(), smart);
+    }
+
+    @Override
+    public int compareTo(Television o) {
+        if(o == null) {
+            throw new IllegalArgumentException("Other tv is null");
+        }
+
+        if(this.getMake().compareTo(o.getMake()) == 0) {
+            if(this.getModel().compareTo(o.getModel()) == 0) {
+                return Integer.compare(this.getScreenSize(), o.getScreenSize());
+            }
+            return this.getModel().compareTo(o.getModel());
+        }
+        return this.getMake().compareTo(o.getMake());
+    }
 }
+
+
